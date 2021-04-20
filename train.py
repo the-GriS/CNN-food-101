4e5e44438ad6c091fb4fb2ea12c41f49632e153d
Stepan <59066411+the-GriS@users.noreply.github.com>
2021-04-20 11:23:54 +0300

    Update train.py

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ def main():
   model = build_model()
 
   model.compile(
-    optimizer=tf.optimizers.Adam(lr=0.01),
+    optimizer=tf.optimizers.Adam(lr=0.0001),
     loss=tf.keras.losses.categorical_crossentropy,
     metrics=[tf.keras.metrics.categorical_accuracy],
   )
