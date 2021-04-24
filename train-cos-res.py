6338409a419af22d76f182d84d372ea696d91e5c
Stepan <59066411+the-GriS@users.noreply.github.com>
2021-04-24 19:44:47 +0300

    Update train-cos-res.py

--- train-cos-res.py
+++ train-cos-res.py
@@ -85,7 +85,7 @@ def main():
     validation_data=validation_dataset,
     callbacks=[
       tf.keras.callbacks.TensorBoard(log_dir),
-      LearningRateScheduler(tf.keras.experimental.CosineDecayRestarts(0.0001, 1000, 2.0, 1.0, 0.0, None),
+      LearningRateScheduler(tf.keras.experimental.CosineDecayRestarts(0.001, 1000, 2.0, 1.0, 0.0, None),
 )
     ]
   )
