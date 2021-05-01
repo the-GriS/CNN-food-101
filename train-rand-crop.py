d71203b882ea1893e7f14da49ec7d2d7c3d424e9
Stepan <59066411+the-GriS@users.noreply.github.com>
2021-05-02 01:21:16 +0300

    Update train-rand-crop.py

--- train-rand-crop.py
+++ train-rand-crop.py
@@ -36,7 +36,7 @@ def parse_proto_example(proto):
   example = tf.io.parse_single_example(proto, keys_to_features)
   example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
   example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.uint8)
-  example['image'] = tf.image.resize(example['image'], tf.constant([400, 400]))
+  example['image'] = tf.image.resize(example['image'], tf.constant([300, 300]))
   return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
 
 
