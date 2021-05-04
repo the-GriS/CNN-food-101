4f1f8995ea44db6eeae9c267697b09d88d282812
Stepan <59066411+the-GriS@users.noreply.github.com>
2021-05-04 17:09:14 +0300

    Update train-fine-tuning.py

--- train-fine-tuning.py
+++ train-fine-tuning.py
@@ -102,7 +102,7 @@ def main():
   log_dir='{}/f101-{}'.format(LOG_DIR, time.time())
   model.fit(
     train_dataset,
-    epochs=30,
+    epochs=19,
     validation_data=validation_dataset,
     callbacks=[
       tf.keras.callbacks.TensorBoard(log_dir)
@@ -118,7 +118,7 @@ def main():
   )
   model.fit(
     train_dataset,
-    epochs=20,
+    epochs=15,
     validation_data=validation_dataset,
     callbacks=[
       tf.keras.callbacks.TensorBoard(log_dir),
