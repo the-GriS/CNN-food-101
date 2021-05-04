aa00aeca953280be8a86dbe84afb53388f378990
Stepan <59066411+the-GriS@users.noreply.github.com>
2021-05-04 17:05:30 +0300

    Update train-fine-tuning.py

--- train-fine-tuning.py
+++ train-fine-tuning.py
@@ -112,7 +112,7 @@ def main():
   unfreeze_model(model)
 
   model.compile(
-    optimizer=tf.optimizers.Adam(lr=1.3e-6),
+    optimizer=tf.optimizers.Adam(lr=1e-7),
     loss=tf.keras.losses.categorical_crossentropy,
     metrics=[tf.keras.metrics.categorical_accuracy],
   )
