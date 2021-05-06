5c15c84c26758585cd449ee3f7d09790e1b06093
the-GriS <grishhenjas@gmail.com>
2021-05-06 10:11:11 +0300

    init

--- train-fine.py
+++ train-fine.py
@@ -97,7 +97,7 @@ def main():
   make_unfreeze_model(model)
   
   model.compile(
-    optimizer=tf.optimizers.Adam(learning_rate = 1e-5),
+    optimizer=tf.optimizers.Adam(learning_rate = 1e-6),
     loss=tf.keras.losses.categorical_crossentropy,
     metrics=[tf.keras.metrics.categorical_accuracy],
   )
