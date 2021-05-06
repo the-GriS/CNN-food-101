69239273431f5a8e5b9eb88b6f1c70c2acc8961e
the-GriS <grishhenjas@gmail.com>
2021-05-06 10:13:42 +0300

    init

--- train-fine.py
+++ train-fine.py
@@ -52,7 +52,7 @@ def create_dataset(filenames, batch_size):
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
   img_aug = tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal")(inputs)
-  x = EfficientNetB0(include_top=False, input_tensor = img_aug,pooling ='avg', weights='imagenet')
+  x = EfficientNetB0(include_top=False, input_tensor = img_aug, pooling ='avg', weights='imagenet')
   x.trainable = False
   x = tf.keras.layers.GlobalAveragePooling2D()(x.output)
   outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
