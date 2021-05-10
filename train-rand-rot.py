1f97b2aaca443f32646de9a39f9360de20f0ed22
Stepan <59066411+the-GriS@users.noreply.github.com>
2021-05-10 21:37:42 +0300

    Update train-rand-rot.py

--- train-rand-rot.py
+++ train-rand-rot.py
@@ -41,11 +41,6 @@ def parse_proto_example(proto):
   example['image'] = tf.image.resize(example['image'], tf.constant([RESIZE_TO, RESIZE_TO]), method='nearest')
   return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
 
-
-def process_data(image, label):
-  img = tf.keras.layers.experimental.preprocessing.RandomRotation(factor=0.05)(image)
-  return img, label
-
   
 def create_dataset(filenames, batch_size):
   """Create dataset from tfrecords file
@@ -54,7 +49,6 @@ def create_dataset(filenames, batch_size):
   """
   return tf.data.TFRecordDataset(filenames)\
     .map(parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE)\
-    .map(process_data)\
     .batch(batch_size)\
     .prefetch(tf.data.AUTOTUNE)
 
